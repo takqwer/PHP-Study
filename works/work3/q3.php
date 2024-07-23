ae0a8b493d036bd507670d3819abb562d00c185f
302A-19\cre <a>
2024-07-23 13:31:55 +0900

    w3 owari

--- works/work3/q3.php
+++ works/work3/q3.php
@@ -2,14 +2,14 @@
 
 // 配列を引数として受け取り、その配列内の数値の平均値を計算して返す関数を完成させなさい
 
-$values = array(1, 2, 3, 4, 5);
+$values = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 $result = average($values);
 echo "平均値: " . $result . "\n";
 
 
 function average($values)
 {
-    $sum = 0;
+    $sum = array_sum($values);
     $count = count($values);
 
     //TODO sumにvaluesの合計値を入れる
